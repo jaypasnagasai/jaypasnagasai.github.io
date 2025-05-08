eda6bff548a26ca28b693740fe43e734e4e810e1
Jayanth Pasupulati <75296693+jaypasnagasai@users.noreply.github.com>
2025-05-08 14:33:48 -0700

    Update metadata.js

--- blog/metadata.js
+++ blog/metadata.js
@@ -1,16 +1,10 @@
 const posts = [
   {
     title: "First",
-    date: "06 April 2024",
+    date: "08 May 2025",
     file: "first.md",
     tags: ["hobby"]
   },
-  {
-    title: "Second",
-    date: "07 April 2024",
-    file: "second.md",
-    tags: ["life"]
-  }
 ];
 
 
